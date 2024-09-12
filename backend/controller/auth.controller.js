8862971f30ef07b950b1210bdad35de21646f596
abhi-906 <87569322+abhi-906@users.noreply.github.com>
2024-09-12 11:45:04 +0530

    auth route setup

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,14 @@
+// import express from "express"
+// import User from "../routes/auth.routes";
+// import User from "../routes/auth.routes";
+
+const signup = (req , res)=>{
+    
+} 
+const login = (req , res)=>{
+
+}
+const logout=(req , res)=>{}
+
+
+// export {signup, login , logout} ;
